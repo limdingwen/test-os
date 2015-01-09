e0e1f5949b3f371af5b0ea87c7eb2e10a48302b9
Lim Ding Wen <limdingwen@icloud.com>
2015-01-09 10:26:19 +0800

    Added compile script.

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Generating 1.44MB blank floppy..."
+dd if=/dev/zero of=boot.img bs=1k count=1440
+echo "OK"
+
+echo "Copying boot1.bin into sector 1..."
+dd if=boot1.bin of=boot.img bs=512 conv=notrunc
+echo "OK"
+
+echo
+echo "Done. Floppy boot.img is located in '.'."
+echo "You can boot this image in any PC x86 emulator,"
+echo "(I use bochs, check out /bochs for the config files) or real PC x86."
