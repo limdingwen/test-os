5cd5bbd35e11636733729a6e3bed215733cc04ea
Lim Ding Wen <limdingwen@icloud.com>
2015-01-09 13:30:14 +0800

    Formatted echos

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo
 echo "Generating 1.44MB blank floppy..."
 dd if=/dev/zero of=boot.img bs=1k count=1440
 echo "OK"
 
+echo
 echo "Copying boot1.bin into sector 1..."
 dd if=boot1.bin of=boot.img bs=512 conv=notrunc
 echo "OK"
