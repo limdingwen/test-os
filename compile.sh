9182d906f8bb1b5aad07f59f346e0ab194d065bd
Lim Ding Wen <limdingwen@icloud.com>
2015-01-10 16:32:33 +0800

    Updated script with rm to ensure complete overwrite. Also changed formatting. Made it silent so that it won't clutter up the screen. You can remove > /dev/null and 2> /dev/null if you want to see the log for debugging etc.

--- compile.sh
+++ compile.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 echo
-echo "Generating 1.44MB blank floppy..."
-dd if=/dev/zero of=boot.img bs=1k count=1440
+echo -n "Deleting boot.img if any to ensure complete overwrite... "
+rm boot.img > /dev/null 2> /dev/null
+echo "OK"
+echo -n "Generating 1.44MB boot.img blank floppy... "
+dd if=/dev/zero of=boot.img bs=1k count=1440 > /dev/null 2> /dev/null
+echo "OK"
+echo -n "Copying sector1.bin into sector 1 of boot.img... "
+dd if=sector1.bin of=boot.img bs=512 conv=notrunc > /dev/null 2> /dev/null
 echo "OK"
 
 echo
-echo "Copying boot1.bin into sector 1..."
-dd if=sector1.bin of=boot.img bs=512 conv=notrunc
-echo "OK"
-
-echo
-echo "Done. Floppy boot.img is located in '.'."
+echo "Done. 1.44MB floppy boot.img is located in '.'."
 echo "You can boot this image in any PC x86 emulator,"
